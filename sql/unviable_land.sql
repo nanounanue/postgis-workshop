dfce10c7d6bdfb39436b5c91182441957034be77
Adolfo De Unánue <nanounanue@gmail.com>
2016-06-19 22:46:41 -0500

    Is `roads` not `road`

--- sql/unviable_land.sql
+++ sql/unviable_land.sql
@@ -1,8 +1,7 @@
 -- All this code is taken from "Spatial Analysis with PostGIS", Leo Hsu and Regina Obe, PGCon2009
 -- URL: https://www.pgcon.org/2009/schedule/events/174.en.html
 
---Delete all land that gets in the way of our roads
--- , water and water ways
+--Delete all land that gets in the way of our roads, water and water ways
 DELETE FROM land
 WHERE EXISTS (
 SELECT h.gid FROM hydrology As h WHERE ST_Intersects(h.the_geom, land.the_geom)
@@ -10,7 +9,7 @@ SELECT h.gid FROM hydrology As h WHERE ST_Intersects(h.the_geom, land.the_geom)
 
 DELETE FROM land
 WHERE EXISTS (
-SELECT r.gid FROM road As r WHERE ST_Intersects(r.the_geom, land.the_geom)
+SELECT r.gid FROM roads As r WHERE ST_Intersects(r.the_geom, land.the_geom)
 );
 
 --Delete all land that is not close enough to a road or water way
@@ -21,5 +20,5 @@ SELECT h.gid FROM hydrology As h WHERE ST_DWithin(h.the_geom,land.the_geom, 2500
 
 DELETE FROM land
 WHERE NOT EXISTS (
-SELECT r.gid FROM road As r WHERE ST_DWithin(r.the_geom,land.the_geom, 3000)
+SELECT r.gid FROM roads As r WHERE ST_DWithin(r.the_geom,land.the_geom, 3000)
 );
